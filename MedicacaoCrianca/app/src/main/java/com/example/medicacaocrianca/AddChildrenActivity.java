502d81c03db95c99585cac9a1a39059429140af1
filipegoncalves9119 <38382740+filipegoncalves9119@users.noreply.github.com>
2021-01-19 15:40:14 +0000

    Initial commit

---
+++ MedicacaoCrianca/app/src/main/java/com/example/medicacaocrianca/AddChildrenActivity.java
@@ -0,0 +1,14 @@
+package com.example.medicacaocrianca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddChildrenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_children);
+    }
+}
