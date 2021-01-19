a9d622a5a953ee1195521590a4c7e4a14dbd7ff4
DiogoSimao10 <52965434+DiogoSimao10@users.noreply.github.com>
2021-01-19 18:53:17 +0000

    Intents
    Adicionados alguns intents

--- MedicacaoCrianca/app/src/main/java/com/example/medicacaocrianca/AdminHomeActivity.java
+++ MedicacaoCrianca/app/src/main/java/com/example/medicacaocrianca/AdminHomeActivity.java
@@ -2,7 +2,9 @@ package com.example.medicacaocrianca;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AdminHomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class AdminHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home);
+    }
+
+    public void AddRoom (View view){
+        Intent intent = new Intent(this, AddRoomActivity.class);
+        AdminHomeActivity.this.startActivity(intent);
     }
 }
--- MedicacaoCrianca/app/src/main/java/com/example/medicacaocrianca/MainActivity.java
+++ MedicacaoCrianca/app/src/main/java/com/example/medicacaocrianca/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.medicacaocrianca;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Currency;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button sendData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendData(View view){
+        Intent intent = new Intent(this, AdminHomeActivity.class);
+        MainActivity.this.startActivity(intent);
     }
 }
