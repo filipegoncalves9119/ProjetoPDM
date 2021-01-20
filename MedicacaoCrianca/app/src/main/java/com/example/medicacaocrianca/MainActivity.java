52c0900225b2597ac7830cb814d99c755709f7ae
filipegoncalves9119 <38382740+filipegoncalves9119@users.noreply.github.com>
2021-01-20 14:28:33 +0000

    login completo

--- MedicacaoCrianca/app/src/main/java/com/example/medicacaocrianca/MainActivity.java
+++ MedicacaoCrianca/app/src/main/java/com/example/medicacaocrianca/MainActivity.java
@@ -1,26 +1,83 @@
 package com.example.medicacaocrianca;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import java.util.Currency;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
-    Button sendData;
+    private Button signIn;
+    private EditText email;
+    private EditText password;
+    private FirebaseAuth firebaseAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.signIn = findViewById(R.id.login_btn_id);
+        this.email = findViewById(R.id.email_text_id);
+        this.password = findViewById(R.id.password_text_id);
+        this.firebaseAuth = FirebaseAuth.getInstance();
+
+        this.signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Login();
+            }
+        });
+
     }
 
-    public void sendData(View view){
-        Intent intent = new Intent(this, AdminHomeActivity.class);
-        MainActivity.this.startActivity(intent);
+    private void Login() {
+        String email = this.email.getText().toString();
+        String password = this.password.getText().toString();
+
+        if(TextUtils.isEmpty(email)){
+            this.email.setError("Enter your e-mail");
+            return;
+        }
+        else if(TextUtils.isEmpty(password)){
+            this.password.setError("Enter your password");
+            return;
+        }
+        firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    if(email.equals("admin@gmail.com")){
+                        Toast.makeText(MainActivity.this,"Sucefully logged in as Admin",Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(MainActivity.this, AdminHomeActivity.class);
+                        startActivity(intent);
+                    }else {
+                        Toast.makeText(MainActivity.this,"Sucefully logged in",Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(MainActivity.this, AddRoomActivity.class);
+                        startActivity(intent);
+                    }
+                }else{
+                    Toast.makeText(MainActivity.this,"E-mail or password incorrect!",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
+
+
 }
